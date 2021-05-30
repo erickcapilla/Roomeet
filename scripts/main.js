36e655c190383736284fc7a61d7f0a3f751a7f34
ErickCapilla <erickpoblano25@gmail.com>
2021-05-30 18:12:52 -0500

    Fixing some things

--- scripts/main.js
+++ scripts/main.js
@@ -1,41 +1,43 @@
-const btn_menu = document.getElementById('btn_menu')
-const arrow = document.getElementById('arrow')
-const menu_card = document.getElementById('menu')
+const btn_menu = document.getElementById("btn_menu");
+const arrow = document.getElementById("arrow");
+const menu_card = document.getElementById("menu");
 
-btn_menu.addEventListener('click', () => {
-  if(!arrow.classList.contains('arrow-down')) {
-    arrow.classList.add('arrow-down')
-    menu_card.classList.add('menu-active')
+btn_menu.addEventListener("click", () => {
+  if (!arrow.classList.contains("arrow-down")) {
+    arrow.classList.add("arrow-down");
+    menu_card.classList.add("menu-active");
   } else {
-    arrow.classList.remove('arrow-down')
-    menu_card.classList.remove('menu-active')
+    arrow.classList.remove("arrow-down");
+    menu_card.classList.remove("menu-active");
   }
-})
+});
 
-let slider = document.getElementById('slider')
-let item = document.querySelectorAll('.items-slider')
+let slider = document.getElementById("slider");
+let item = document.querySelectorAll(".items-slider");
 
 function Next() {
-  let itemFirst = document.querySelectorAll('.items-slider')[0]
-  slider.style.marginLeft = "-200%"
-  slider.style.transition = "all 0.5s"
+  let itemFirst = document.querySelectorAll(".items-slider")[0];
+  slider.style.marginLeft = "-200%";
+  slider.style.transition = "all 0.5s";
   setTimeout(() => {
-    slider.style.transition = "none"
-    slider.insertAdjacentElement('beforeend', itemFirst)
-    slider.style.marginLeft = "-100%"
-  }, 500)
+    slider.style.transition = "none";
+    slider.insertAdjacentElement("beforeend", itemFirst);
+    slider.style.marginLeft = "-100%";
+  }, 500);
 }
 
 setInterval(() => {
-  Next()
-}, 5000)
+  Next();
+}, 5000);
 
-const items_btn = document.querySelectorAll('#item')
+const items_btn = document.querySelectorAll("#item");
 
-items_btn.forEach(item => {
-  item.addEventListener('click', () => {
-    if(!window.scroll(0, document.body.scrollHeight)){
+items_btn.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (!window.scroll(0, document.body.scrollHeight)) {
       window.scroll(0, document.body.scrollHeight);
+      arrow.classList.remove("arrow-down");
+      menu_card.classList.remove("menu-active");
     }
-  })
-})
+  });
+});
